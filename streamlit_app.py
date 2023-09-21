85de25d5e773650a49a20a12ee6a7ef44bd9c91e
utopia-maizuru <144981022+utopia-maizuru@users.noreply.github.com>
2023-09-21 04:32:19 +0000

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,8 +10,9 @@ st.set_page_config(page_title="タイトル", layout="wide")
 # タイトルを設定
 st.title("おみくじアプリ")
 
-if  st.button("おみくじを引く")
-results = ["大吉","中吉","小吉","吉","凶","大凶"]
-result = random.choice(results)
-st.write(f"結果:{result}")
+if  st.button("おみくじを引く"):
+    results = ["大吉","中吉","小吉","吉","凶","大凶"]
+    result = random.choice(results)
+    st.write(f"結果:{result}")
+
 
